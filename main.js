42369bfcab098f47208a15c52295f5856904ad13
eeshalteluri <eeshalteluri@gmail.com>
2025-05-24 22:30:40 +0530

    Fixed image import typo

--- main.js
+++ main.js
@@ -350,11 +350,11 @@ try {
         <!-- Scroller Section-->
         <section id="scroller" class="sm:h-20 py-4 bg-secondary flex flex-col sm:flex-row justify-center items-center overflow-x-auto">
             <div class="w-full sm:w-[85%] sm:mx-auto flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4 sm:gap-10">
-                <img src="/images/scroller/Notion.png" alt="Notion" class="w-auto h-10 object-cover" />
-                <img src="/images/scroller/Medium.png" alt="Medium" class="w-auto h-10 object-cover" />
-                <img src="/images/scroller/Trello.png" alt="Trello" class="w-auto h-10 object-cover" />
-                <img src="/images/scroller/Slack.png" alt="Slack" class="w-auto h-10 object-cover" />
-                <img src="/images/scroller/Mailchimp.png" alt="Mailchimp" class="w-auto h-10 object-cover" />
+                <img src="/images/Scroller/Notion.png" alt="Notion" class="w-auto h-10 object-cover" />
+                <img src="/images/Scroller/Medium.png" alt="Medium" class="w-auto h-10 object-cover" />
+                <img src="/images/Scroller/Trello.png" alt="Trello" class="w-auto h-10 object-cover" />
+                <img src="/images/Scroller/Slack.png" alt="Slack" class="w-auto h-10 object-cover" />
+                <img src="/images/Scroller/Mailchimp.png" alt="Mailchimp" class="w-auto h-10 object-cover" />
             </div>
         </section>
 
